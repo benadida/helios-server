5ee278a949484b92d71955dcbcf709ebf7ae8faa
Ben Adida <ben@adida.net>
2010-05-02 21:33:23 -0700

    continuing updates for port to pure django

--- urls.py
+++ urls.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^auth/', include('auth.urls')),
     (r'^helios/', include('helios.urls')),
+    
+    # SHOULD BE REPLACED BY APACHE STATIC PATH
+    (r'static/auth/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/auth/media'}),
+    (r'static/helios/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/helios/media'}),
+    (r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/server_ui/media'}),
+
     (r'^', include('server_ui.urls')),
-)
+
+    )
