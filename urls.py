82b4bc904ba54f78975a1f72f1e1e45ba00177ef
Ben Adida <ben@adida.net>
2010-05-04 17:39:42 -0700

    continued porting

--- urls.py
+++ urls.py
@@ -7,8 +7,10 @@ urlpatterns = patterns(
     '',
     (r'^auth/', include('auth.urls')),
     (r'^helios/', include('helios.urls')),
-    
+
     # SHOULD BE REPLACED BY APACHE STATIC PATH
+    (r'booth/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/heliosbooth'}),
+
     (r'static/auth/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/auth/media'}),
     (r'static/helios/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/helios/media'}),
     (r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.ROOT_PATH + '/server_ui/media'}),
