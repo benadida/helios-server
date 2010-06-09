4163d26f94c1f84b1d2d055ff17b597419f04722
Ben Adida <ben@adida.net>
2010-06-09 12:06:46 -0700

    added reset file

---
+++ reset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dropdb helios
+createdb helios
+python manage.py syncdb
+echo "from auth.models import User; User.update_or_create(user_type='password',user_id='benadida',info={'password':'test'})" | python manage.py shell
