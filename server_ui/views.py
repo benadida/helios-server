43fdd9f1d752c9b8b06d2350982da25080baf920
Ben Adida <ben@adida.net>
2010-08-08 16:14:55 -0700

    updates to look and feel

--- server_ui/views.py
+++ server_ui/views.py
@@ -17,6 +17,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpRespons
 
 from django.conf import settings
 
+import copy
+import auth.views as auth_views
 
 def get_election():
   return None
@@ -25,9 +27,31 @@ def home(request):
   # load the featured elections
   featured_elections = Election.get_featured()
   
+  user = get_user(request)
   create_p = can_create_election(request)
-  
-  return render_template(request, "index", {'elections': featured_elections, 'create_p':create_p})
+
+  if create_p:
+    elections_administered = Election.get_by_user_as_admin(user)
+  else:
+    elections_administered = None
+
+  if user:
+    elections_voted = Election.get_by_user_as_voter(user)
+  else:
+    elections_voted = None
+ 
+  auth_systems = copy.copy(settings.AUTH_ENABLED_AUTH_SYSTEMS)
+  try:
+    auth_systems.remove('password')
+  except: pass
+
+  login_box = auth_views.login_box_raw(request, return_url="/", auth_systems=auth_systems)
+
+  return render_template(request, "index", {'elections': featured_elections,
+                                            'elections_administered' : elections_administered,
+                                            'elections_voted' : elections_voted,
+                                            'create_p':create_p,
+                                            'login_box' : login_box})
   
 def about(request):
   return HttpResponse(request, "about")
