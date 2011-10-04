31f36b6b40e17651f90cbaf52dbda7b71db320fb
Ben Adida <ben@adida.net>
2011-10-04 06:23:50 -0700

    ff 3.6 compat

--- helios/urls.py
+++ helios/urls.py
@@ -9,6 +9,7 @@ urlpatterns = None
 
 urlpatterns = patterns('',
   (r'^$', home),
+  (r'^test$', test),
   (r'^autologin$', admin_autologin),
   (r'^testcookie$', test_cookie),
   (r'^testcookie_2$', test_cookie_2),
