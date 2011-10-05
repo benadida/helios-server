50d26c18256debaadbc4fc7433478d7413f86da8
Ben Adida <ben@adida.net>
2011-10-04 22:26:07 -0700

    added tests for randomness fix

--- helios/tests.py
+++ helios/tests.py
@@ -568,6 +568,11 @@ class ElectionBlackboxTests(WebTest):
         response = self.app.post("/helios/elections/%s/encrypt-ballot" % election_id, {
                 'answers_json': utils.to_json([[1]])})
         self.assertContains(response, "answers")
+
+        # parse it as an encrypted vote with randomness, and make sure randomness is there
+        the_ballot = utils.from_json(response.testbody)
+        assert the_ballot['answers'][0].has_key('randomness'), "no randomness"
+        assert len(the_ballot['answers'][0]['randomness']) == 2, "not enough randomness"
         
         # parse it as an encrypted vote, and re-serialize it
         ballot = datatypes.LDObject.fromDict(utils.from_json(response.testbody), type_hint='legacy/EncryptedVote')
