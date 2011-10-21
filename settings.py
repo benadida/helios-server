aacc1560b320e5dfc61a038a6a8fc8f4c0e0b6ac
Ben Adida <ben@adida.net>
2011-10-20 19:01:19 -0700

    added settings file

---
+++ settings.py
@@ -0,0 +1,182 @@
+
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'helios'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Los_Angeles'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'replaceme'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+ROOT_PATH = os.path.dirname(__file__)
+TEMPLATE_DIRS = (
+    ROOT_PATH,
+    os.path.join(ROOT_PATH, 'templates')
+)
+
+INSTALLED_APPS = (
+#    'django.contrib.auth',
+#    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    ## needed for queues
+    'djcelery',
+    ## needed for schema migration
+    'south',
+    ## HELIOS stuff
+    'auth',
+    'helios',
+    'server_ui',
+)
+
+##
+## HELIOS
+##
+
+
+MEDIA_ROOT = ROOT_PATH + "media/"
+
+# a relative path where voter upload files are stored
+VOTER_UPLOAD_REL_PATH = "voters/%Y/%m/%d"
+
+
+# Change your email settings
+DEFAULT_FROM_EMAIL = 'ben@adida.net'
+DEFAULT_FROM_NAME = 'Ben for Helios'
+SERVER_EMAIL = '%s <%s>' % (DEFAULT_FROM_NAME, DEFAULT_FROM_EMAIL)
+
+LOGIN_URL = '/auth/'
+LOGOUT_ON_CONFIRMATION = True
+
+# The two hosts are here so the main site can be over plain HTTP
+# while the voting URLs are served over SSL.
+URL_HOST = "http://localhost:8000"
+
+# IMPORTANT: you should not change this setting once you've created
+# elections, as your elections' cast_url will then be incorrect.
+SECURE_URL_HOST = "https://localhost:8443"
+
+# this additional host is used to iframe-isolate the social buttons,
+# which usually involve hooking in remote JavaScript, which could be
+# a security issue. Plus, if there's a loading issue, it blocks the whole
+# page. Not cool.
+SOCIALBUTTONS_URL_HOST= "http://127.0.0.1:8000"
+
+# election stuff
+SITE_TITLE = 'Helios Election Server'
+
+# FOOTER links
+FOOTER_LINKS = []
+FOOTER_LOGO = False
+
+WELCOME_MESSAGE = "This is the default message"
+
+AUTH_TEMPLATE_BASE = "server_ui/templates/base.html"
+HELIOS_TEMPLATE_BASE = "server_ui/templates/base.html"
+HELIOS_ADMIN_ONLY = False
+HELIOS_VOTERS_UPLOAD = True
+HELIOS_VOTERS_EMAIL = True
+
+# authentication systems enabled
+AUTH_ENABLED_AUTH_SYSTEMS = ['password','facebook','twitter', 'google', 'yahoo']
+AUTH_DEFAULT_AUTH_SYSTEM = None
+
+# facebook
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_KEY = ''
+FACEBOOK_API_SECRET = ''
+
+# twitter
+TWITTER_API_KEY = ''
+TWITTER_API_SECRET = ''
+TWITTER_USER_TO_FOLLOW = 'heliosvoting'
+TWITTER_REASON_TO_FOLLOW = "we can direct-message you when the result has been computed in an election in which you participated"
+
+# the token for Helios to do direct messaging
+TWITTER_DM_TOKEN = {"oauth_token": "", "oauth_token_secret": "", "user_id": "", "screen_name": ""}
+
+# LinkedIn
+LINKEDIN_API_KEY = ''
+LINKEDIN_API_SECRET = ''
+
+# email server
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
+# set up logging
+import logging
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = '%(asctime)s %(levelname)s %(message)s'
+)
+
+# set up django-celery
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "guest"
+BROKER_PASSWORD = "guest"
+BROKER_VHOST = "/"
+
+import djcelery
+djcelery.setup_loader()
