49673382c64a28ccef6d3e414903a6eb7838e2b5
Ben Adida <ben@adida.net>
2011-12-20 08:29:50 -0800

    reenabled dummy bigint in case where browser is not doing the crypto

--- heliosbooth/js/jscrypto/bigint.dummy.js
+++ heliosbooth/js/jscrypto/bigint.dummy.js
@@ -8,7 +8,7 @@
 // A wrapper for java.math.BigInteger with some appropriate extra functions for JSON and 
 // generally being a nice JavaScript object.
 
-BigInt = Class.extend({
+BigIntDummy = Class.extend({
 	init: function(value, radix) {
 	    if (radix != 10)
 		throw "in dummy, only radix=10, here radix=" + radix;
@@ -59,71 +59,27 @@ BigInt = Class.extend({
   
 });
 
-BigInt.ready_p = false;
+BigIntDummy.use_applet = false;
 
-BigInt.use_applet = false;
+BigIntDummy.is_dummy = true;
+BigIntDummy.in_browser = false;
 
-BigInt.is_dummy = true;
-
-BigInt.fromJSONObject = function(s) {
-  return new BigInt(s, 10);
+BigIntDummy.fromJSONObject = function(s) {
+  return new BigIntDummy(s, 10);
 };
 
-BigInt.fromInt = function(i) {
-  return BigInt.fromJSONObject("" + i);
+BigIntDummy.fromInt = function(i) {
+  return BigIntDummy.fromJSONObject("" + i);
 };
 
-// Set up the pointer to the applet if necessary, and some
-// basic Big Ints that everyone needs (0, 1, 2, and 42)
-BigInt._setup = function() {
-  try {
-    BigInt.ZERO = new BigInt("0",10);
-    BigInt.ONE = new BigInt("1",10);
-    BigInt.TWO = new BigInt("2",10);
-    BigInt.FORTY_TWO = new BigInt("42",10);
-  
-    BigInt.ready_p = true;
-  } catch (e) {
-    // not ready
-    // count how many times we've tried
-    if (this.num_invocations == null)
-      this.num_invocations = 0;
+BigIntDummy.ZERO = new BigIntDummy("0",10);
+BigIntDummy.ONE = new BigIntDummy("1",10);
+BigIntDummy.TWO = new BigIntDummy("2",10);
+BigIntDummy.FORTY_TWO = new BigIntDummy("42",10);
 
-    this.num_invocations += 1;
+BigIntDummy.ready_p = true;
 
-    if (this.num_invocations > 5) {
-      if (BigInt.setup_interval)
-        window.clearInterval(BigInt.setup_interval);
-      
-      if (BigInt.setup_fail) {
-        BigInt.setup_fail();
-      } else {
-        alert('bigint failed!');
-      }
-    }
-    return;
-  }
-  
-  if (BigInt.setup_interval)
-    window.clearInterval(BigInt.setup_interval);
-    
-  if (BigInt.setup_callback)
-    BigInt.setup_callback();
-};
-
-BigInt.setup = function(callback, fail_callback) {
-  if (callback)
-    BigInt.setup_callback = callback;
-  
-  if (fail_callback)
-    BigInt.setup_fail = fail_callback;
-  
-  BigInt.setup_interval = window.setInterval("BigInt._setup()", 1000);
+BigIntDummy.setup = function(callback, fail_callback) {
+  console.log("using dummy bigint");
+  callback();
 }
-
-// .onload instead of .ready, as I don't think the applet is ready until onload.
-// FIXME: something wrong here in the first load
-$(document).ready(function() {
-	BigInt.use_applet = false;
-});
-
