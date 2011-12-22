d0ce5f4f88a9e7fb58dc0f0cf5974e95f00e984a
Ben Adida <ben@adida.net>
2011-12-22 07:17:32 -0800

    back to urandom

--- helios/views.py
+++ helios/views.py
@@ -514,9 +514,9 @@ def get_randomness(request, election):
   get some randomness to sprinkle into the sjcl entropy pool
   """
   return {
-    # temporary fix
-    #"randomness" : base64.b64encode(os.urandom(32))
-    "randomness" : base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
+    # back to urandom, it's fine
+    "randomness" : base64.b64encode(os.urandom(32))
+    #"randomness" : base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
     }
 
 @json
