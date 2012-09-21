21e931cb2238e51b1c17eb3e4d809a60f8d9fcb5
Kostas Papadimitriou <kpap@grnet.gr>
2012-09-21 21:27:45 +0300

    Fix url typo

--- helios/election_urls.py
+++ helios/election_urls.py
@@ -10,7 +10,7 @@ from helios.views import *
 
 urlpatterns = patterns('',
     (r'^$', one_election),
-    
+
     # edit election params
     (r'^/edit$', one_election_edit),
     (r'^/schedule$', one_election_schedule),
@@ -25,16 +25,16 @@ urlpatterns = patterns('',
     (r'^/trustees/new$', new_trustee),
     (r'^/trustees/add-helios$', new_trustee_helios),
     (r'^/trustees/delete$', delete_trustee),
-    
+
     # trustee pages
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/home$', trustee_home),
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/sendurl$', trustee_send_url),
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/keygenerator$', trustee_keygenerator),
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/check-sk$', trustee_check_sk),
-    (r'^/trustees/(?P<trustee_uuid>[^/]+)/upoad-pk$', trustee_upload_pk),
+    (r'^/trustees/(?P<trustee_uuid>[^/]+)/upload-pk$', trustee_upload_pk),
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/decrypt-and-prove$', trustee_decrypt_and_prove),
     (r'^/trustees/(?P<trustee_uuid>[^/]+)/upload-decryption$', trustee_upload_decryption),
-    
+
     # election voting-process actions
     (r'^/view$', one_election_view),
     (r'^/result$', one_election_result),
@@ -55,20 +55,20 @@ urlpatterns = patterns('',
     (r'^/save_questions$', one_election_save_questions),
     (r'^/register$', one_election_register),
     (r'^/freeze$', one_election_freeze), # includes freeze_2 as POST target
-    
+
     # computing tally
     (r'^/compute_tally$', one_election_compute_tally),
     (r'^/combine_decryptions$', combine_decryptions),
-    
+
     # casting a ballot before we know who the voter is
     (r'^/cast$', one_election_cast),
     (r'^/cast_confirm$', one_election_cast_confirm),
     (r'^/password_voter_login$', password_voter_login),
     (r'^/cast_done$', one_election_cast_done),
-    
+
     # post audited ballot
     (r'^/post-audited-ballot', post_audited_ballot),
-    
+
     # managing voters
     (r'^/voters/$', voter_list),
     (r'^/voters/upload$', voters_upload),
@@ -78,7 +78,7 @@ urlpatterns = patterns('',
     (r'^/voters/email$', voters_email),
     (r'^/voters/(?P<voter_uuid>[^/]+)$', one_voter),
     (r'^/voters/(?P<voter_uuid>[^/]+)/delete$', voter_delete),
-    
+
     # ballots
     (r'^/ballots/$', ballot_list),
     (r'^/ballots/(?P<voter_uuid>[^/]+)/all$', voter_votes),
