f6fa486d17c19378354e6e52b36b5cdb28af99d8
Ben Adida <ben@adida.net>
2012-09-30 13:17:55 -0700

    trying a new unicode csv parser

--- helios/models.py
+++ helios/models.py
@@ -26,7 +26,8 @@ from auth.jsonfield import JSONField
 from helios.datatypes.djangofield import LDObjectField
 
 import csv, copy
-  
+import unicodecsv
+
 class HeliosModel(models.Model, datatypes.LDObjectContainer):
   class Meta:
     abstract = True
@@ -653,7 +654,8 @@ class VoterFile(models.Model):
     else:
       voter_stream = open(self.voter_file.path, "rU")
 
-    reader = unicode_csv_reader(voter_stream)
+    #reader = unicode_csv_reader(voter_stream)
+    reader = unicodecsv.reader(voter_stream)
 
     for voter_fields in reader:
       # bad line
