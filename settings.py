3db22adfca8dbf0cbe2098edd526f0410e5be0a3
Kostas Papadimitriou <kpap@grnet.gr>
2012-10-12 15:37:34 +0300

    Django 1.3/1.4 fix

--- settings.py
+++ settings.py
@@ -61,8 +61,8 @@ SECRET_KEY = get_from_env('SECRET_KEY', 'replaceme')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
