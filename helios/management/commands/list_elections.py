a4afd672f3c94fd1bd43753634b5de1b8ea9da01
Georgios D. Tsoukalas <gtsouk@cslab.ece.ntua.gr>
2012-10-17 00:38:45 +0300

    add list elections and voter logins mgmt commands

---
+++ helios/management/commands/list_elections.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from helios import utils as helios_utils
+from helios.models import *
+from heliosauth.models import *
+
+class Command(BaseCommand):
+    args = ''
+    help = 'List elections'
+
+    def handle(self, *args, **options):
+        for e in Election.objects.all():
+            print e.uuid
