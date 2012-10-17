b3cb51844fe1b8f3eeaa8eb548d5358ee27cb34e
Georgios D. Tsoukalas <gtsouk@cslab.ece.ntua.gr>
2012-10-17 15:31:50 +0300

    list trustee and voter urls management commands

---
+++ helios/management/commands/list_voter_urls.py
@@ -0,0 +1,31 @@
+"""
+verify cast votes that have not yet been verified
+
+Ben Adida
+ben@adida.net
+2010-05-22
+"""
+import csv, datetime
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from helios import utils as helios_utils
+from helios.models import *
+
+class Command(BaseCommand):
+    args = ''
+    help = 'List the voter login urls'
+
+    def handle(self, *args, **options):
+        if args:
+	    election = Election.objects.get(uuid=args[0])
+            voters = Voter.objects.filter(election=election)
+        else:
+            voters = Voter.objects.all()
+
+        for v in voters:
+            print v.election.uuid, v.get_quick_login_url()
+        # once broken out of the while loop, quit and wait for next invocation
+        # this happens when there are no votes left to verify
+
