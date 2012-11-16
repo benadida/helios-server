b1acd32dd14c6d40a5c3422a97695723036b8d56
Georgios D. Tsoukalas <gtsouk@cslab.ece.ntua.gr>
2012-11-16 13:19:18 +0200

    more info columns to list_elections cmd

--- helios/management/commands/list_elections.py
+++ helios/management/commands/list_elections.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
@@ -5,15 +7,29 @@ from helios import utils as helios_utils
 from helios.models import *
 from heliosauth.models import *
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+def strforce(thing):
+    if isinstance(thing, unicode):
+        return thing.encode('utf-8')
+    return thing
 
 class Command(BaseCommand):
     args = ''
     help = 'List elections'
 
     def handle(self, *args, **options):
+        print ','.join(('uuid', 'όνομα', 'διαχειριστής', 'ίδρυμα',
+                        'εκλέκτορες', 'ψηφίσαντες', 'έναρξη', 'λήξη'))
         for e in Election.objects.all():
-            print e.uuid, e.name, '--', e.admins.all()[0].pretty_name,
-            print '--', e.institution.name
+            uuid = strforce(e.uuid)
+            name = strforce(e.name)
+            admin = strforce(e.admins.all()[0].pretty_name)
+            institution = strforce(e.institution.name)
+            voted_count = str(e.voted_count())
+            voter_count = str(e.voter_set.count())
+            start = e.voting_starts_at
+            start = start.strftime("%Y-%m-%d %H:%M:%S") if start else '-'
+            end = e.voting_ended_at
+            end = end.strftime("%Y-%m-%d %H:%M:%S") if end else '-'
+            print ','.join((uuid, name, admin, institution,
+                            voter_count, voted_count, start, end))
+
