84745023f6a3114f7ac2e4e42fb1e8f76c7b8bf4
Kostas Papadimitriou <kpap@grnet.gr>
2013-01-16 12:30:54 +0200

    Include missing file

---
+++ heliosauth/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class ChangePasswordForm(forms.Form):
+    password = forms.CharField(_('Current password'), widget=forms.PasswordInput)
+    new_password = forms.CharField(_('New password'), widget=forms.PasswordInput)
+    new_password_cofirm = forms.CharField(_('New password confirm'), widget=forms.PasswordInput)
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(ChangePasswordForm, self).__init__(*args, **kwargs)
+
+    def save(self):
+        user = self.user
+        user.info['password'] = self.cleaned_data['new_password'].strip()
+        user.save()
+
+    def clean(self):
+        cl = super(ChangePasswordForm, self).clean()
+        if not self.user.info['password'] == self.cleaned_data.get('password'):
+            raise forms.ValidationError(_('Invalid password'))
+        if not self.cleaned_data.get('new_password') == \
+           self.cleaned_data.get('new_password_cofirm'):
+            raise forms.ValidationError(_('Passwords don\'t match'))
+        return cl
+
+
