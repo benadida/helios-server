5e293529cd8e58cd7eb62d38ef28c7af9f6cb75a
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2013-09-09 14:33:18 +0300

    rename obsolete ADMIN_MEDIA_URL -> STATIC_URL

--- settings.py
+++ settings.py
@@ -58,7 +58,8 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+#ADMIN_MEDIA_PREFIX = '/media/'
+STATIC_URL = '/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = get_from_env('SECRET_KEY', 'replaceme')
