219d7d867bb5bf1a27b4584396803e02a2b776d4
Kostas Papadimitriou <kpap@grnet.gr>
2013-09-15 18:26:34 +0300

    Update translations

--- update-translations.sh
+++ update-translations.sh
@@ -4,7 +4,7 @@ for d in zeus helios heliosauth server_ui; do
 cd $d;
 DJANGO_SETTINGS_MODULE=settings;
 PYTHONPATH=..;
-django-admin makemessages -l el;
+django-admin makemessages -l el -e .html -e .txt;
 django-admin compilemessages;
 cd ..;
 done;
