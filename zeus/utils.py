7cc237fc76a28bb0e3c3801df8ef4c2a4462b66b
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2013-09-20 13:16:31 +0300

    decalize()/undecalize() utils

--- zeus/utils.py
+++ zeus/utils.py
@@ -69,3 +69,103 @@ def sanitize_mobile_number(num):
         return num[-12:]
     raise Exception("Invalid number")
 
+
+def decalize(string, sep='-', chunk=2):
+    if not isinstance(string, str):
+        m = "argument must be an 'str', not %r" % type(string)
+        raise ValueError(m)
+    slist = []
+    s = ''
+    i = 0
+    for z, c in enumerate(string):
+        o = ord(c)
+        if o < 32 or o > 127:
+            m = ("index %d: Can only decalize printable ASCII characters "
+                 "in range 32-127, not character %d(\\x%x)")
+            m = m % (z, o, o)
+            raise ValueError(m)
+        s += "%02d" % (ord(c) - 32)
+        i += 1
+        if i == chunk:
+            slist.append(s)
+            s = ''
+            i = 0
+
+    if s:
+        slist.append(s)
+
+    return sep.join(slist)
+
+
+def undecalize(string):
+    i = 2
+    s = ''
+    d = 0
+    for z, c in enumerate(string):
+        if not c.isdigit():
+            continue
+        i -= 1
+        d *= 10
+        d += int(c, 10)
+        if not i:
+            d += 32
+            if d > 127:
+                m = "index %d: invalid ASCII code %d > 127" % (z, d)
+                raise ValueError(m)
+            s += chr(d)
+            d = 0
+            i = 2
+
+    if i != 2:
+        m = "Input has an odd number of decimal digits: %d" % (z + 1)
+        raise ValueError(m)
+
+    return s
+
+
+def test_decalize():
+    import random
+    alphabet = 'abcdefghkmnpqrstuvwxyzABCDEFGHKLMNPQRSTUVWXYZ23456789'
+
+    N = 1000
+    for i in xrange(N):
+        s = ''
+        for j in xrange(12):
+            s += random.choice(alphabet)
+        dec = decalize(s, sep='-', chunk=2)
+        #print s, '-', dec
+        undec = undecalize(dec)
+        if undec != s:
+            m = "%s %s %s %s" % (i, s, dec, undec)
+            raise AssertionError("decalize-undecalize mismatch: %s" % m)
+
+    if decalize("") != undecalize(""):
+        raise AssertionError()
+
+    if decalize(" ") != "00":
+        raise AssertionError()
+
+    if undecalize("00") != " ":
+        raise AssertionError()
+
+    if undecalize("95") != "\x7f":
+        raise AssertionError()
+
+    decalize_tests = ["\x1f", "\x80"]
+    for t in decalize_tests:
+        try:
+            decalize(t)
+        except ValueError as e:
+            pass
+        else:
+            raise AssertionError("Decalize(%s) failed to fail" % t)
+
+    undecalize_tests = ["9012-3", "42019609"]
+    for t in undecalize_tests:
+        try:
+            undecalize(t)
+        except ValueError as e:
+            pass
+        else:
+            raise AssertionError("Undecalized(%s) failed to fail" % t)
+#
