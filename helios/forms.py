286d424448a4740cd9b798b4a218fb3b0e638bc9
Pieter Maene <pieter@maene.eu>
2014-03-04 15:20:25 +0100

    Minor change in the create election form

--- helios/forms.py
+++ helios/forms.py
@@ -8,8 +8,8 @@ from widgets import *
 from fields import *
 
 class ElectionForm(forms.Form):
-  short_name = forms.SlugField(max_length=25, help_text='No spaces, will be part of the URL for your election, e.g. my-club-2010')
   name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':60}), help_text='The pretty name for your election, e.g. My Club 2010 Election')
+  short_name = forms.SlugField(label="Slug", max_length=25, help_text='No spaces, will be part of the URL for your election, e.g. my-club-2010')
   description = forms.CharField(max_length=2000, widget=forms.Textarea(attrs={'cols': 70, 'wrap': 'soft'}), required=False)
   election_type = forms.ChoiceField(label="Type", choices = Election.ELECTION_TYPES)
   use_voter_aliases = forms.BooleanField(required=False, initial=False, help_text='If selected, voter identities will be replaced with aliases, e.g. "V12", in the ballot tracking center')
