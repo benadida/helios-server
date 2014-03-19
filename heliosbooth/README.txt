704501d7d9ea86fcc9ed66350a2225195415be62
Maureen Lyons <maureen@digital-loom.com>
2014-03-19 16:32:32 -0400

    Voting Booth Changes Convert to SCSS, Remove in-line styles from templates, rename CSS files

--- heliosbooth/README.txt
+++ heliosbooth/README.txt
@@ -2,5 +2,3 @@ The Helios Booth
 ================
 
 LICENSE: this code is released under the GPL v3 or later.
-
----
---
+++ heliosbooth/config.rb
@@ -0,0 +1,8 @@
+#
+# This file is only needed for Compass/Sass integration. If you are not using
+# Compass, you may safely ignore or delete this file.
+#
+
+# Location of the resources.
+css_dir         = "css"
+sass_dir        = "sass"
