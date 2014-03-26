f3b4aeba029263b6b9491212e7a39456fe42a290
Giorgos Tsakalos <tsakalos@grnet.gr>
2014-03-26 16:28:44 +0200

    added boolfield management_p on heliosauth.models.User

---
+++ heliosauth/migrations/0002_auto__add_field_user_management_p.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'User.management_p'
+        db.add_column('heliosauth_user', 'management_p',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'User.management_p'
+        db.delete_column('heliosauth_user', 'management_p')
+
+
+    models = {
+        'heliosauth.user': {
+            'Meta': {'unique_together': "(('user_type', 'user_id'),)", 'object_name': 'User'},
+            'admin_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ecounting_account': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('heliosauth.jsonfield.JSONField', [], {}),
+            'institution': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['zeus.Institution']", 'null': 'True'}),
+            'management_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'superadmin_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'token': ('heliosauth.jsonfield.JSONField', [], {'null': 'True'}),
+            'user_id': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'user_type': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'zeus.institution': {
+            'Meta': {'object_name': 'Institution'},
+            'ecounting_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['heliosauth']
--- zeus/auth.py
+++ zeus/auth.py
@@ -95,7 +95,7 @@ def manager_or_superadmin_required(func):
     def wrapper(request, *args, **kwargs):
         if not (request.zeususer._user.superadmin_p
                 or request.zeususer.is_manager):
-            raise PersmissionDenied("Superadmin or manager required")
+            raise PermissionDenied("Superadmin or manager required")
         return func(request, *args, **kwargs)
     return wrapper
 
