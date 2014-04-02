a8fe1ad4d98262af2107cc2143b6dba9f1d31aab
Giorgos Tsakalos <tsakalos@grnet.gr>
2014-04-02 12:10:31 +0300

    added migration for unique user_id of User model

---
+++ heliosauth/migrations/0004_auto__add_unique_user_user_id.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'User', fields ['user_id']
+        db.create_unique('heliosauth_user', ['user_id'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'User', fields ['user_id']
+        db.delete_unique('heliosauth_user', ['user_id'])
+
+
+    models = {
+        'heliosauth.user': {
+            'Meta': {'unique_together': "(('user_type', 'user_id'),)", 'object_name': 'User'},
+            'admin_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ecounting_account': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('heliosauth.jsonfield.JSONField', [], {}),
+            'institution': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['zeus.Institution']", 'null': 'True'}),
+            'is_disabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'management_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'superadmin_p': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'token': ('heliosauth.jsonfield.JSONField', [], {'null': 'True'}),
+            'user_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'user_type': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'zeus.institution': {
+            'Meta': {'object_name': 'Institution'},
+            'ecounting_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_disabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['heliosauth']
