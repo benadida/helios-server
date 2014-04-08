b3a3dc2aa1638c8c90986b63b9adbe354a1ae97a
Pieter Maene <pieter@maene.eu>
2014-04-08 19:31:57 +0200

    Improved the interface of the election verifier

--- urls.py
+++ urls.py
@@ -3,23 +3,17 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
 
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns('',
     (r'^helios_auth/', include('helios_auth.urls')),
     (r'^helios/', include('helios.urls')),
 
     # SHOULD BE REPLACED BY APACHE STATIC PATH
-    (r'booth/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT_PATH + '/heliosbooth'}),
-    (r'verifier/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT_PATH + '/heliosverifier'}),
+    (r'booth/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ROOT_PATH + '/helios_booth'}),
+    (r'verifier/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ROOT_PATH + '/helios_verifier'}),
 
-    (r'static/helios_auth/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT_PATH + '/helios_auth/media'}),
-    (r'static/helios/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT_PATH + '/helios/media'}),
-    (r'static/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.ROOT_PATH + '/server_ui/media'}),
+    (r'static/helios_auth/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ROOT_PATH + '/helios_auth/media'}),
+    (r'static/helios/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ROOT_PATH + '/helios/media'}),
+    (r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ROOT_PATH + '/server_ui/media'}),
 
     (r'^', include('server_ui.urls')),
     (r'^bulletin_board/', include('bulletin_board.urls')),
