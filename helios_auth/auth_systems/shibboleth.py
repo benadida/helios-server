27282c1e7cb1790e29336dce9a4371ef3e982c29
Pieter Maene <pieter@maene.eu>
2014-04-13 16:27:23 +0200

    Some Shibboleth fixes

--- helios_auth/auth_systems/shibboleth.py
+++ helios_auth/auth_systems/shibboleth.py
@@ -7,13 +7,6 @@ import urllib
 
 from django.conf import settings
 
-SHIBBOLETH_SESSION_KEY = 'Shib_Session_Index'
-SHIBBOLETH_PERSON_KEY = 'Shib_Person_uid'
-SHIBBOLETH_FIRST_NAME = 'Shib_Person_givenName'
-SHIBBOLETH_LAST_NAME = 'Shib_Person_surname'
-SHIBBOLETH_EMAIL = 'Shib_Person_mail'
-
-
 def _get_service_url():
     # FIXME current URL
     from helios_auth.views import after
@@ -29,12 +22,15 @@ def get_auth_url(request, redirect_url):
 
 
 def get_user_info_after_auth(request):
-    if not os.environ[SHIBBOLETH_SESSION_KEY]:
+    if not os.environ[settings.SHIBBOLETH_SESSION_KEY]:
         return None
 
-    user_id = os.environ[SHIBBOLETH_PERSON_KEY]
-    email = os.environ[SHIBBOLETH_EMAIL]
-    name = "%s %s" % (os.environ[SHIBBOLETH_FIRST_NAME], os.environ[SHIBBOLETH_LAST_NAME])
+    user_id = os.environ[settings.SHIBBOLETH_PERSON_KEY]
+    email = os.environ[settings.SHIBBOLETH_EMAIL]
+
+    name = ''
+    if settings.SHIBBOLETH_FIRST_NAME != '' and settings.SHIBBOLETH_LAST_NAME != '':
+        name = "%s %s" % (os.environ[settings.SHIBBOLETH_FIRST_NAME], os.environ[settings.SHIBBOLETH_LAST_NAME])
 
     return {'type': 'shibboleth', 'user_id': user_id, 'name': name, 'info': {'email': email}, 'token': {}}
 
