26a7e9af13641dcb5de83585d6a7b6d1001d2cab
Kostas Papadimitriou <kpap@grnet.gr>
2014-04-23 22:10:56 +0300

    simple/parties booth fix

--- zeus/static/booth/js/booth.js
+++ zeus/static/booth/js/booth.js
@@ -164,12 +164,8 @@ BOOTH.setup_election = function(raw_json) {
     if (BOOTH.election.election_module == "simple") {
       prepend = 1;
     }
-    if (!BOOTH.election.module_params.count_empty_question) {
-        prepend = 0;
-    } else {
-        prepend = 1;
-        data['answers'].unshift("Empty Party Choice");
-    }
+    data['answers'].unshift("Empty Party Choice");
+    if (!BOOTH.election.module_params.count_empty_question) { prepend = 0 }
     answers_index = parseInt(answers_index) + data['answers'].length + prepend;
   });
 
