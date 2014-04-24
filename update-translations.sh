72d41ce3ec883a2c0998a4218e52ee07cf8745fd
Kostas Papadimitriou <kpap@grnet.gr>
2014-04-24 16:02:50 +0300

    remove source line reference from translation catalogs

--- update-translations.sh
+++ update-translations.sh
@@ -4,9 +4,9 @@ export PYTHONPATH=`pwd`
 export DJANGO_SETTINGS_MODULE=settings;
 for d in zeus helios heliosauth server_ui account_administration; do
   cd $d;
-  django-admin makemessages -l el -e .html -e .txt;
+  django-admin makemessages --no-location -l el -e .html -e .txt;
   #django-admin makemessages -l en -e .html -e .txt;
   cd ..;
 done;
 
-python manage.py makeboothmessages -l en -l el -e .html -e .js;
+python manage.py makeboothmessages --no-location -l en -l el -e .html -e .js;
