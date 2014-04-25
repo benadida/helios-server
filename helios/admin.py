1d6c1ed78685bdf7093b63126e0e3e1a4c6d92b8
Shirlei Chaves <shirlei@gmail.com>
2014-04-25 16:17:35 -0300

    Add Election model to admin app
    Initial intent is to allow setting featured_p

--- helios/admin.py
+++ helios/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from helios.models import Voter
+from helios.models import Voter, Election
 
 
 class VoterAdmin(admin.ModelAdmin):	
-    fields = ('user', 'vote_hash', 'cast_at', 'voter_email')
-    readonly_fields = ('user', 'vote_hash', 'cast_at', 'voter_email')
-    list_display = ('user', 'vote_hash', 'cast_at', 'voter_email')
+	fields = ('user', 'vote_hash', 'cast_at', 'voter_email')
+	readonly_fields = ('user', 'vote_hash', 'cast_at', 'voter_email')
+	list_display = ('user', 'vote_hash', 'cast_at', 'voter_email')
+
+
+class ElectionAdmin(admin.ModelAdmin):
+	fields = ('admin', 'short_name', 'name', 'election_type',
+	'openreg', 'use_voter_aliases', 'randomize_answer_order', 
+	'frozen_at', 'tallying_started_at', 'tallying_finished_at',
+	'help_email', 'election_info_url', 'result', 'featured_p')
+	readonly_fields = ('admin', 'short_name', 'name', 'election_type',
+	'openreg', 'use_voter_aliases', 'randomize_answer_order', 
+	'frozen_at', 'tallying_started_at', 'tallying_finished_at', 'help_email',
+	'election_info_url', 'result')
+	list_display = ('admin', 'name', 'election_type', 'featured_p')
 
 admin.site.register(Voter, VoterAdmin)
+admin.site.register(Election, ElectionAdmin)
