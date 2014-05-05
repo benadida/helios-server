cc773cf4d81fdaf7fe453c7fd6bd2fa71ac2a5d3
Shirlei Chaves <shirlei@gmail.com>
2014-05-05 10:07:49 -0300

    Change and comment out initial user

--- reset.sh
+++ reset.sh
@@ -3,4 +3,4 @@ dropdb helios
 createdb helios
 python manage.py syncdb
 python manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
+#echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='shirlei@gmail.com', info={'name':'Shirlei Chaves'})" | python manage.py shell
