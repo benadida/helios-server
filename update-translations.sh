88f567171a9821cacd0a46d4466e32dff0978b3e
Kostas Papadimitriou <kpap@grnet.gr>
2014-05-13 11:37:58 +0300

    Translate utils fixes
    properly resolve django-admin location

--- update-translations.sh
+++ update-translations.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-export PYTHONPATH=`pwd`
+export PYTHONPATH=`pwd`;
 export DJANGO_SETTINGS_MODULE=settings;
 export DJANGO_ADMIN=`which django-admin`;
-if [ -f `which django-admin.py` ]; then
-    DJANGO_ADMIN=`which django-admin.py`;
+export VENV_DJANGO_ADMIN=`which django-admin.py`;
+
+if [ -n "$VENV_DJANGO_ADMIN" ] 
+then
+    DJANGO_ADMIN=$VENV_DJANGO_ADMIN;
 fi
 
 for d in zeus helios heliosauth server_ui account_administration; do
