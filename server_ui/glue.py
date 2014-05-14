cad5087328bd462d6db8758929d212c4b34fae6c
Shirlei Chaves <shirlei@gmail.com>
2014-05-14 16:49:12 -0300

    Add blank space in email message

--- server_ui/glue.py
+++ server_ui/glue.py
@@ -16,13 +16,13 @@ def vote_cast_send_message(user, voter, election, cast_vote, **kwargs):
   # prepare the message
   subject = _("%(election_name)s - vote cast") % {'election_name' : election.name}
 
-  body = _('You have successfully cast a vote in\n\n%(election_name)s') % {'election_name' : election.name}
+  body = _('You have successfully cast a vote in\n\n%(election_name)s\n') % {'election_name' : election.name}
 
-  body += _('Your ballot is archived at:\n\n%(cast_url)s') % {'cast_url' : helios.views.get_castvote_url(cast_vote)}
+  body += _('Your ballot is archived at:\n\n%(cast_url)s\n') % {'cast_url' : helios.views.get_castvote_url(cast_vote)}
   
  
   if election.use_voter_aliases:
-    body += _('This election uses voter aliases to protect your privacy.'
+    body += _('\nThis election uses voter aliases to protect your privacy.'
 'Your voter alias is :\n\n%(voter_alias)s') % {'voter_alias' : voter.alias}
 
   body += """
