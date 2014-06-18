ec3e4432f02076ec57cd17b2ad1ec48c3a1ec72c
Shirlei Chaves <shirlei@gmail.com>
2014-06-18 18:45:20 -0300

    Add timezone support

--- helios/models.py
+++ helios/models.py
@@ -11,6 +11,7 @@ from django.utils import simplejson
 from django.utils.translation import ugettext as _
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils.timezone import utc
 
 import datetime, logging, uuid, random, io
 import bleach
@@ -334,7 +335,7 @@ class Election(HeliosModel):
     or failing that the date voting was extended until, or failing that the date voting is scheduled to end at.
     """
     voting_end = self.voting_ended_at or self.voting_extended_until or self.voting_ends_at
-    return (voting_end != None and datetime.datetime.utcnow() >= voting_end) or self.encrypted_tally
+    return (voting_end != None and datetime.datetime.utcnow().replace(tzinfo=utc) >= voting_end) or self.encrypted_tally
 
   @property
   def issues_before_freeze(self):
--- settings.py
+++ settings.py
@@ -58,6 +58,7 @@ if get_from_env('DATABASE_URL', None):
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/Sao_Paulo'
+USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
