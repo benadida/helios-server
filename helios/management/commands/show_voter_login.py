1aab3be811fc748e241e576dcbf19da8272b113b
Giorgos Tsakalos <tsakalos@grnet.gr>
2014-07-03 16:13:13 +0300

    fix show_voter_login
    voter.election deprecated
    update with voter.poll.election

--- helios/management/commands/show_voter_login.py
+++ helios/management/commands/show_voter_login.py
@@ -19,7 +19,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for v in Voter.objects.filter(voter_email=args[0]):
-            print v.election.uuid, v.get_quick_login_url()
+            print "election uuid : %s" % v.poll.election.uuid
+            print "poll uuid : %s" % v.poll.uuid
+            print "login url : %s" % v.get_quick_login_url()
         # once broken out of the while loop, quit and wait for next invocation
         # this happens when there are no votes left to verify
 
