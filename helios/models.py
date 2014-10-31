ac9b52975bbee254f2622f520d62be5e3e32e703
Giorgos Tsakalos <tsakalos@grnet.gr>
2014-10-31 16:52:58 +0200

    update mail to admins system
    send mail when election is closed

--- helios/models.py
+++ helios/models.py
@@ -468,6 +468,9 @@ class Election(ElectionTasks, HeliosModel, ElectionFeatures):
         self.voting_ended_at = datetime.datetime.now()
         self.save()
         self.logger.info("Voting closed")
+        subject = _("Election closed")
+        msg = _("Election closed")
+        self.send_msg_to_admins(msg=msg, subject=subject)
 
     def freeze(self):
         for poll in self.polls.all():
