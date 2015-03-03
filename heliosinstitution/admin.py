4c83ced7eb85568ec228a4157165cae8fc7f3170
Shirlei Chaves <shirlei@gmail.com>
2015-03-02 23:07:18 -0300

    Add admin class

--- heliosinstitution/admin.py
+++ heliosinstitution/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from heliosinstitution.models import Institution
+
+
 # Register your models here.
+class HeliosInstitutionAdmin(admin.ModelAdmin):	
+	fields = ('short_name', 'name', 'address', 'main_phone', 'sec_phone', 'mngt_email')
+	list_display = ('name', 'short_name', 'main_phone', 'mngt_email')
+
+
+admin.site.register(Institution, HeliosInstitutionAdmin)
