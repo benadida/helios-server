85a37015731fb182a19342a1ed36ca29c0de7227
Shirlei Chaves <shirlei@gmail.com>
2015-03-16 11:24:49 -0300

    Add initial views/urls for institution user management

--- heliosinstitution/urls.py
+++ heliosinstitution/urls.py
@@ -12,4 +12,8 @@ from views import *
 
 urlpatterns = patterns('',
     (r'^manage_users$', manage_users),
+    (r'^(?P<institution_id>\d+)/delegate_institution_admin$', delegate_institution_admin),
+    (r'^(?P<institution_id>\d+)/revoke_institution_admin$', revoke_institution_admin),
+    (r'^(?P<institution_id>\d+)/delegate_election_admin$', delegate_election_admin),
+    (r'^(?P<institution_id>\d+)/revoke_election_admin$', revoke_election_admin),
 )
--- heliosinstitution/views.py
+++ heliosinstitution/views.py
@@ -1,10 +1,63 @@
-from view_utils import *
+from django.views.decorators.http import require_http_methods
 
+
+from helios_auth.security import *
+from heliosinstitution.models import InstitutionUserProfile
+
+
+from view_utils import *
 
 def home(request):
     pass
 
+@login_required
+def manage_users(request):  
+    user = get_user(request)
+    institution = user.authuserprofile_set.get().institution
+    users = InstitutionUserProfile.objects.filter(institution=institution)
 
-def manage_users(request):
-  if request.method == "GET":
-    return render_template(request, "manage_users")
+    if request.method == "GET":
+        return render_template(request, "manage_users", {
+            "users": users,
+            "institution": institution,
+        })
+
+
+@login_required
+@require_http_methods(["POST",])
+def delegate_institution_admin(request, institution_id):
+    """
+    Delegate an user to administer institution
+    """
+    user = get_user(request)
+    pass
+
+
+@login_required
+@require_http_methods([ "POST,"])
+def revoke_institution_admin(request, institution_id):
+    """
+    Revoke an user as institution admin
+    """
+    user = get_user(request)
+    pass
+
+
+@login_required
+@require_http_methods(["POST",])
+def delegate_election_admin(request, institution_id):
+    """
+    Enable an user to administer elections
+    """
+    user = get_user(request)
+    pass
+
+
+@login_required
+@require_http_methods(["POST",])
+def revoke_election_admin(request, institution_id):
+    """
+    Revoke an user as elections admin
+    """
+    user = get_user(request)
+    pass
