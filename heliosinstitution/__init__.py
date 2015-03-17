27d77c0edad1f9f97b02fdca61f613cab1b10a67
Shirlei Chaves <shirlei@gmail.com>
2015-03-17 18:46:17 -0300

    Change update permissions/group after migrantion
    Now considering just heliosinstitution app

--- heliosinstitution/__init__.py
+++ heliosinstitution/__init__.py
@@ -15,34 +15,43 @@ from django.contrib.auth.models import Group, Permission
 import models 
 
 verbosity = 2
-def update_permissions_after_migration(**kwargs):
-    """
-    Permissions must exist in app model, otherwise an error
-    will be thrown
-    """
-    heliosinstitution_group_permissions = {
-        "Institution Admin" : [
-            "delegate_institution_mngt",
-            "revoke_institution_mngt",
-            "delegate_election_mngt",
-            "revoke_election_mngt"
-        ],
+
+def update_permissions_after_migration(app, **kwargs):
+
+    from django.conf import settings
+    from django.db.models import get_app, get_models
+    from django.contrib.auth.management import create_permissions
+    
+    create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
+
+    if app == "heliosinstitution":
+        """
+        Permissions must exist in app model, otherwise an error
+        will be thrown
+        """
+        heliosinstitution_group_permissions = {
+            "Institution Admin" : [
+                "delegate_institution_mngt",
+                "revoke_institution_mngt",
+                "delegate_election_mngt",
+                "revoke_election_mngt"
+            ],
 #        "Election Admin": [
 #            "admin_election",
 #        ]
-        
-    }
-    if verbosity>0:
-        print "Initialising data post_migrate"
-    for group in heliosinstitution_group_permissions:
-        role, created = Group.objects.get_or_create(name=group)
-        if verbosity > 1 and created:
-            print 'Creating group', group
-            for perm in heliosinstitution_group_permissions[group]:
-                role.permissions.add(Permission.objects.get(codename=perm))
-                if verbosity > 1:
-                    print 'Permitting', group, 'to', perm
-                    role.save()  
+            
+        }
+        if verbosity>0:
+            print "Initialising data post_migrate"
+        for group in heliosinstitution_group_permissions:
+            role, created = Group.objects.get_or_create(name=group)
+            if verbosity > 1 and created:
+                print 'Creating group', group
+                for perm in heliosinstitution_group_permissions[group]:
+                    role.permissions.add(Permission.objects.get(codename=perm))
+                    if verbosity > 1:
+                        print 'Permitting', group, 'to', perm
+                        role.save()  
 
 
 post_migrate.connect(update_permissions_after_migration)
