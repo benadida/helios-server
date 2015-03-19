f73e26c8e477667c21db937b0c076023956470dd
Pieter Maene <pieter@maene.eu>
2015-03-19 18:06:52 +0100

    Cleaned up the statistics views

--- helios/stats_views.py
+++ helios/stats_views.py
@@ -26,8 +26,7 @@ def require_admin(request):
 
 def home(request):
     user = require_admin(request)
-    num_votes_in_queue = CastVote.objects.filter(
-        invalidated_at=None, verified_at=None).count()
+    num_votes_in_queue = CastVote.objects.filter(invalidated_at=None, verified_at=None).count()
     return render_template(request, 'stats', {'num_votes_in_queue': num_votes_in_queue})
 
 
@@ -51,8 +50,12 @@ def elections(request):
     elections_paginator = Paginator(elections, limit)
     elections_page = elections_paginator.page(page)
 
-    return render_template(request, "stats_elections", {'elections': elections_page.object_list, 'elections_page': elections_page,
-                                                        'limit': limit})
+    return render_template(request, "stats_elections", {
+        'elections': elections_page.object_list,
+        'elections_paginator' : elections_paginator,
+        'elections_page': elections_page,
+        'limit': limit
+    })
 
 
 def recent_votes(request):
@@ -60,8 +63,7 @@ def recent_votes(request):
 
     # elections with a vote in the last 24 hours, ordered by most recent cast vote time
     # also annotated with number of votes cast in last 24 hours
-    elections_with_votes_in_24hours = Election.objects.filter(voter__castvote__cast_at__gt=datetime.datetime.utcnow(
-    ) - datetime.timedelta(days=1)).annotate(last_cast_vote=Max('voter__castvote__cast_at'), num_recent_cast_votes=Count('voter__castvote')).order_by('-last_cast_vote')
+    elections_with_votes_in_24hours = Election.objects.filter(voter__castvote__cast_at__gt=datetime.datetime.utcnow() - datetime.timedelta(days=1)).annotate(last_cast_vote=Max('voter__castvote__cast_at'), num_recent_cast_votes=Count('voter__castvote')).order_by('-last_cast_vote')
 
     return render_template(request, "stats_recent_votes", {'elections': elections_with_votes_in_24hours})
 
@@ -71,7 +73,10 @@ def recent_problem_elections(request):
 
     # elections left unfrozen older than 1 day old (and younger than 10 days
     # old, so we don't go back too far)
-    elections_with_problems = Election.objects.filter(frozen_at=None, created_at__gt=datetime.datetime.utcnow(
-    ) - datetime.timedelta(days=10), created_at__lt=datetime.datetime.utcnow() - datetime.timedelta(days=1))
+    elections_with_problems = Election.objects.filter(
+        frozen_at=None,
+        created_at__gt=datetime.datetime.utcnow() - datetime.timedelta(days=10),
+        created_at__lt=datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    )
 
     return render_template(request, "stats_problem_elections", {'elections': elections_with_problems})
