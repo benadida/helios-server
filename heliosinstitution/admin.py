edce6c827ccced000fb3257ea8715ec86d30231f
Shirlei Chaves <shirlei@gmail.com>
2015-03-29 23:52:49 -0300

    Alter user creation

--- heliosinstitution/admin.py
+++ heliosinstitution/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
+
 
 from heliosinstitution.models import Institution, InstitutionUserProfile
 
@@ -12,5 +16,14 @@ class InstitutionUserProfileAdmin(admin.ModelAdmin):
     list_display = ('helios_user', 'email', 'institution')
 
 
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('username',)}),
+        (_('Permissions'), {'fields': ('groups', )}),
+    )
+
+
 admin.site.register(InstitutionUserProfile, InstitutionUserProfileAdmin)
 admin.site.register(Institution, HeliosInstitutionAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
