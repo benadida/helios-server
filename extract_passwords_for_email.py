a0ffd6abfbe3177ea9c381be68fc2abf70d3019d
Pieter Maene <pieter@maene.eu>
2015-04-14 12:43:47 +0200

    Fixed and renamed the password extraction script

--- extract_passwords_for_email.py
+++ extract_passwords_for_email.py
@@ -2,15 +2,13 @@
 # extract voter_id and passwords for a particular email address
 # may return many rows, if they all have the same email address
 #
-# python extract-passwords-for-email.py <election_uuid> <email_address>
+# python extract_passwords_for_email.py <election_uuid> <email_address>
 #
 
-from django.core.management import setup_environ
-import settings
+import os
 import sys
-import csv
 
-setup_environ(settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from helios.models import *
 
@@ -19,8 +17,7 @@ email = sys.argv[2]
 
 csv_output = csv.writer(sys.stdout)
 
-voters = Election.objects.get(
-    uuid=election_uuid).voter_set.filter(voter_email=email)
+voters = Election.objects.get(uuid=election_uuid).voter_set.filter(voter_email=email)
 
 for voter in voters:
     csv_output.writerow([voter.voter_login_id, voter.voter_password])
