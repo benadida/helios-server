3790c660fd65a21ae6d57b1c7b2f877fbdc9c475
Shirlei Chaves <shirlei@gmail.com>
2015-05-07 22:44:46 -0300

    Add session to admin page

--- heliosinstitution/admin.py
+++ heliosinstitution/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 
-from heliosinstitution.models import Institution, InstitutionUserProfile
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+        list_display = ['session_key', '_session_data', 'expire_date']
 
 
 class HeliosInstitutionAdmin(admin.ModelAdmin):	
@@ -27,3 +31,4 @@ admin.site.register(InstitutionUserProfile, InstitutionUserProfileAdmin)
 admin.site.register(Institution, HeliosInstitutionAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Session, SessionAdmin)
