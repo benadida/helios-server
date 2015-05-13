5fe2abb9c14b3320c193b63b676ec54ee8636be5
Kostas Papadimitriou <kpap@grnet.gr>
2015-05-13 16:20:12 +0300

    Update tests

--- zeus/tests/unit.py
+++ zeus/tests/unit.py
@@ -1,7 +1,14 @@
+from functools import partial
 from django.test import TestCase
 
-from zeus.utils import parse_q_param, get_voters_filters
+from zeus.utils import parse_q_param, get_filters, VOTER_TABLE_HEADERS, \
+    VOTER_SEARCH_FIELDS, VOTER_BOOL_KEYS_MAP, VOTER_EXTRA_HEADERS
 
+
+get_voters_filters = lambda inp: get_filters(inp, VOTER_TABLE_HEADERS,
+                                                  VOTER_SEARCH_FIELDS,
+                                                  VOTER_BOOL_KEYS_MAP,
+                                                  VOTER_EXTRA_HEADERS)
 
 class TestUtils(TestCase):
 
