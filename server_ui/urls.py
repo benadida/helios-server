4b06838bf13c90997ebcc60c3421045f15a66ed3
Shirlei Chaves <shirlei@gmail.com>
2015-07-29 11:00:51 -0300

    Remove unncessary import

--- server_ui/urls.py
+++ server_ui/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import *
 from django.conf import settings
 
 from views import *
-from heliosinstitution import views as institution_views
 
 urlpatterns = patterns('',
   (r'^$', home),
