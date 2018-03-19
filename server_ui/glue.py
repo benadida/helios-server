42cfd2e408de3dbced4088a064a179361d089937
Shirlei Chaves <shirlei@gmail.com>
2018-03-19 10:00:25 -0300

    Make cast vote email a template

--- server_ui/glue.py
+++ server_ui/glue.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.utils.translation import ugettext as _
+from helios.view_utils import render_template_raw
 
 import helios.views, helios.signals
 
@@ -14,23 +15,18 @@ import views
 def vote_cast_send_message(user, voter, election, cast_vote, **kwargs):
   ## FIXME: this doesn't work for voters that are not also users
   # prepare the message
-  subject = _("[vote cast] - %(election_name)s") % {'election_name' : election.name}
-
-  body = _('You have successfully cast a vote in\n\n%(election_name)s\n') % {'election_name' : election.name}
-
-  body += _('Your ballot is archived at:\n\n%(cast_url)s\n') % {'cast_url' : helios.views.get_castvote_url(cast_vote)}
+  subject_template = 'email/cast_vote_subject.txt'
+  body_template = 'email/cast_vote_body.txt'
   
-  body += '\n\n'
-
-  if election.use_voter_aliases:
-    body += _('\nThis election uses voter aliases to protect your privacy.'
-'Your voter alias is :\n\n%(voter_alias)s') % {'voter_alias' : voter.alias}
-
-  body += """
-
---
-%s
-""" % unicode(settings.SITE_TITLE)
+  extra_vars = {
+    'election' : election,
+    'voter': voter,
+    'cast_vote': cast_vote,
+    'cast_vote_url': helios.views.get_castvote_url(cast_vote),
+    'custom_subject' : _("[vote cast] - %(election_name)s") % {'election_name' : election.name}
+  }
+  subject = render_template_raw(None, subject_template, extra_vars)
+  body = render_template_raw(None, body_template, extra_vars)
   
   # send it via the notification system associated with the auth system
   user.send_message(subject, body)
