fb77fd1726f6d13a14a1e2f342b00d5d59b9597f
Marco Ciotola <848222@stud.unive.it>
2019-02-18 22:31:32 +0100

    [DJ1.10] list of urlpatterns is a list of url() instead of a patterns() result

--- helios/stats_urls.py
+++ helios/stats_urls.py
@@ -4,15 +4,14 @@ Helios URLs for Election related stuff
 Ben Adida (ben@adida.net)
 """
 
-from django.conf.urls import *
+from django.conf.urls import url
 
-from helios.stats_views import *
+from helios.stats_views import (home, force_queue, elections, recent_problem_elections, recent_votes)
 
-urlpatterns = patterns(
-    '',
-    (r'^$', home),
-    (r'^force-queue$', force_queue),
-    (r'^elections$', elections),
-    (r'^problem-elections$', recent_problem_elections),
-    (r'^recent-votes$', recent_votes),
-)
+urlpatterns = [
+    url(r'^$', home),
+    url(r'^force-queue$', force_queue),
+    url(r'^elections$', elections),
+    url(r'^problem-elections$', recent_problem_elections),
+    url(r'^recent-votes$', recent_votes),
+]
--- helios/urls.py
+++ helios/urls.py
@@ -1,35 +1,32 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include
+from django.conf.urls import url, include
 
 from views import *
 
-urlpatterns = patterns('',
-  (r'^autologin$', admin_autologin),
-  (r'^testcookie$', test_cookie),
-  (r'^testcookie_2$', test_cookie_2),
-  (r'^nocookies$', nocookies),
-  (r'^stats/', include('helios.stats_urls')),
+urlpatterns = [
+  url(r'^autologin$', admin_autologin),
+  url(r'^testcookie$', test_cookie),
+  url(r'^testcookie_2$', test_cookie_2),
+  url(r'^nocookies$', nocookies),
+  url(r'^stats/', include('helios.stats_urls')),
 
   # election shortcut by shortname
-  (r'^e/(?P<election_short_name>[^/]+)$', election_shortcut),
-  (r'^e/(?P<election_short_name>[^/]+)/vote$', election_vote_shortcut),
+  url(r'^e/(?P<election_short_name>[^/]+)$', election_shortcut),
+  url(r'^e/(?P<election_short_name>[^/]+)/vote$', election_vote_shortcut),
 
   # vote shortcut
-  (r'^v/(?P<vote_tinyhash>[^/]+)$', castvote_shortcut),
+  url(r'^v/(?P<vote_tinyhash>[^/]+)$', castvote_shortcut),
   
   # trustee login
-  (r'^t/(?P<election_short_name>[^/]+)/(?P<trustee_email>[^/]+)/(?P<trustee_secret>[^/]+)$', trustee_login),
+  url(r'^t/(?P<election_short_name>[^/]+)/(?P<trustee_email>[^/]+)/(?P<trustee_secret>[^/]+)$', trustee_login),
   
   # election
-  (r'^elections/params$', election_params),
-  (r'^elections/verifier$', election_verifier),
-  (r'^elections/single_ballot_verifier$', election_single_ballot_verifier),
-  (r'^elections/new$', election_new),
-  (r'^elections/administered$', elections_administered),
-  (r'^elections/voted$', elections_voted),
+  url(r'^elections/params$', election_params),
+  url(r'^elections/verifier$', election_verifier),
+  url(r'^elections/single_ballot_verifier$', election_single_ballot_verifier),
+  url(r'^elections/new$', election_new),
+  url(r'^elections/administered$', elections_administered),
+  url(r'^elections/voted$', elections_voted),
   
-  (r'^elections/(?P<election_uuid>[^/]+)', include('helios.election_urls')),
-  
-)
-
-
+  url(r'^elections/(?P<election_uuid>[^/]+)', include('helios.election_urls')),
+]
