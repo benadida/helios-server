4f322ad48de52ca717c2ed8c5a098b00e52978c5
Marco Ciotola <848222@stud.unive.it>
2019-02-19 01:06:11 +0100

    [DJ1.9] Add used django.contrib apps in installed apps

--- settings.py
+++ settings.py
@@ -144,11 +144,11 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-#    'django.contrib.auth',
-#    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'djangosecure',
     'django.contrib.sessions',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     ## needed for queues
     'djcelery',
     'kombu.transport.django',
