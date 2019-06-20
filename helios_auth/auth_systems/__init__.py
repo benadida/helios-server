d5adffab4056e2a68c45b41a19d0c31b5608401d
Marco Ciotola <848222@stud.unive.it>
2019-06-20 17:46:52 +0200

    Import only enabled auth systems

--- helios_auth/auth_systems/__init__.py
+++ helios_auth/auth_systems/__init__.py
@@ -1,19 +1,50 @@
+from django.conf import settings
+
+_enabled = settings.AUTH_ENABLED_AUTH_SYSTEMS or None
+def _is_enabled(system):
+    return _enabled is not None or system in _enabled
 
 AUTH_SYSTEMS = {}
 
-import twitter, password, cas, facebook, google, yahoo, linkedin, clever
-import ldapauth, shibboleth
+if _is_enabled('twitter'):
+    import twitter
+    AUTH_SYSTEMS['twitter'] = twitter
 
-AUTH_SYSTEMS['twitter'] = twitter
-AUTH_SYSTEMS['linkedin'] = linkedin
-AUTH_SYSTEMS['password'] = password
-AUTH_SYSTEMS['cas'] = cas
-AUTH_SYSTEMS['facebook'] = facebook
-AUTH_SYSTEMS['google'] = google
-AUTH_SYSTEMS['yahoo'] = yahoo
-AUTH_SYSTEMS['clever'] = clever
-AUTH_SYSTEMS['ldap'] = ldapauth
-AUTH_SYSTEMS['shibboleth'] = shibboleth
+if _is_enabled('linkedin'):
+    import linkedin
+    AUTH_SYSTEMS['linkedin'] = linkedin
+
+if _is_enabled('password'):
+    import password
+    AUTH_SYSTEMS['password'] = password
+
+if _is_enabled('cas'):
+    import cas
+    AUTH_SYSTEMS['cas'] = cas
+
+if _is_enabled('facebook'):
+    import facebook
+    AUTH_SYSTEMS['facebook'] = facebook
+
+if _is_enabled('google'):
+    import google
+    AUTH_SYSTEMS['google'] = google
+
+if _is_enabled('yahoo'):
+    import yahoo
+    AUTH_SYSTEMS['yahoo'] = yahoo
+
+if _is_enabled('clever'):
+    import clever
+    AUTH_SYSTEMS['clever'] = clever
+
+if _is_enabled('ldap'):
+    import ldapauth
+    AUTH_SYSTEMS['ldap'] = ldapauth
+
+if _is_enabled('shibboleth'):
+    import shibboleth
+    AUTH_SYSTEMS['shibboleth'] = shibboleth
 
 # not ready
 #import live
