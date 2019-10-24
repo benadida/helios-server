48c577ca39aa40bbe7163ad6e17866ec9aef5019
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2019-10-24 16:55:55 +1300

    201910241648

--- settings.py
+++ settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'helios',
-        # 'USER': xxxx',
-        # 'PASSWORD': 'xxxx',
+        'USER': 'warwickmcnaughton',
+        'PASSWORD': '1629',
     }
 }
 
@@ -306,8 +306,8 @@ AUTH_ENABLED_AUTH_SYSTEMS = get_from_env('AUTH_ENABLED_AUTH_SYSTEMS', 'google').
 AUTH_DEFAULT_AUTH_SYSTEM = get_from_env('AUTH_DEFAULT_AUTH_SYSTEM', None)
 
 # google
-GOOGLE_CLIENT_ID = get_from_env('GOOGLE_CLIENT_ID', 'xxxx')
-GOOGLE_CLIENT_SECRET = get_from_env('GOOGLE_CLIENT_SECRET', 'xxxx')
+GOOGLE_CLIENT_ID = get_from_env('GOOGLE_CLIENT_ID', '739721212934-9anb7pq77h6nglknafiiq6svb50a73mr.apps.googleusercontent.com')
+GOOGLE_CLIENT_SECRET = get_from_env('GOOGLE_CLIENT_SECRET', ***REMOVED***)
 
 # facebook
 FACEBOOK_APP_ID = get_from_env('FACEBOOK_APP_ID','')
