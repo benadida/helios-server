6803af4b7bbe7e56c90de4f131b4e9b33ee90eaf
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2019-10-26 10:57:37 +1300

    201910261057

--- venv/lib/python3.7/site-packages/rollbar/contrib/django/__init__.py
+++
@@ -1,5 +0,0 @@
-from django.db.models import query
-from rollbar import blacklisted_local_types
-
-# QuerySet objects will potentially execute SQL if you call repr() on them
-blacklisted_local_types.extend([query.QuerySet])
--- venv/lib/python3.7/site-packages/rollbar/test/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest2
-
-
-SNOWMAN = b'\xe2\x98\x83'
-SNOWMAN_UNICODE = SNOWMAN.decode('utf8')
-
-
-class BaseTest(unittest2.TestCase):
-    pass
