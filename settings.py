c32b839b737c6127a741bc8cfd82de1da00600c3
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2020-02-23 08:42:39 +1300

    Update BASE_DIR

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ import os, json
 ##############################
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # a massive hack to see if we're testing, in which case we use different settings
 import sys
