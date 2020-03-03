a4e9a5aa83e2b7fad66cd2076d87485df34a123c
Marco Ciotola <848222@stud.unive.it>
2020-03-03 14:13:18 +0100

    [py3] str-bytes comparisons

--- helios/crypto/electionalgs.py
+++ helios/crypto/electionalgs.py
@@ -6,6 +6,7 @@ Ben Adida
 """
 import datetime
 import uuid
+import logging
 
 from helios.utils import to_json
 from . import algs
@@ -322,17 +323,28 @@ class EncryptedVote(HeliosObject):
     FIELDS = ['encrypted_answers', 'election_hash', 'election_uuid']
 
     def verify(self, election):
-        # right number of answers
-        if len(self.encrypted_answers) != len(election.questions):
+        # correct number of answers
+        # noinspection PyUnresolvedReferences
+        n_answers = len(self.encrypted_answers) if self.encrypted_answers is not None else 0
+        n_questions = len(election.questions) if election.questions is not None else 0
+        if n_answers != n_questions:
+            logging.error(f"Incorrect number of answers ({n_answers}) vs questions ({n_questions})")
             return False
 
         # check hash
-        if self.election_hash != election.hash:
-            # print "%s / %s " % (self.election_hash, election.hash)
+        # noinspection PyUnresolvedReferences
+        our_election_hash = self.election_hash if isinstance(self.election_hash, str) else self.election_hash.decode()
+        actual_election_hash = election.hash if isinstance(election.hash, str) else election.hash.decode()
+        if our_election_hash != actual_election_hash:
+            logging.error(f"Incorrect election_hash {our_election_hash} vs {actual_election_hash} ")
             return False
 
         # check ID
-        if self.election_uuid != election.uuid:
+        # noinspection PyUnresolvedReferences
+        our_election_uuid = self.election_uuid if isinstance(self.election_uuid, str) else self.election_uuid.decode()
+        actual_election_uuid = election.uuid if isinstance(election.uuid, str) else election.uuid.decode()
+        if our_election_uuid != actual_election_uuid:
+            logging.error(f"Incorrect election_uuid {our_election_uuid} vs {actual_election_uuid} ")
             return False
 
         # check proofs on all of answers
