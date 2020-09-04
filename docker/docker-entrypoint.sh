f45311267da5a376745b7a26fb95d5565c6269d7
Shirlei Chaves <shirlei@gmail.com>
2020-09-04 11:09:50 -0300

    Set to True instead of 'true' ...

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 echo "from django.contrib.auth.models import User; 
 from django.contrib.auth.hashers import make_password; 
-User.objects.create(is_staff='true',is_superuser='true', username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password=make_password('$DJANGO_SUPERUSER_PASSWORD'))" | python manage.py shell
+User.objects.create(is_staff=True,is_superuser=True, username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password=make_password('$DJANGO_SUPERUSER_PASSWORD'))" | python manage.py shell
 
 service supervisor start
 
