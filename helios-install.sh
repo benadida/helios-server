fc58edfc893d5814dc8fb0d6224ddad37508e289
root <root@debian-ihjbatzd.athens-dev.al.vps.tecnico.ulisboa.pt>
2020-12-09 18:35:08 +0000

    Update helios install script

--- helios-install.sh
+++ helios-install.sh
@@ -7,33 +7,56 @@ sudo apt-get install -y build-essential
 sudo apt-get update
 sudo apt-get install -y python-setuptools
 sudo apt-get install -y python-pip
+sudo pip install --upgrade pip
 sudo pip install virtualenv
-sudo virtualenv venv;sudo virtualenv -p /usr/bin/python2.7 venv; source venv/bin/activate
+sudo virtualenv venv && sudo chmod 777 -R $(pwd)venv/
+source $(pwd)/venv/bin/activate
 
-#sudo /bin/sh -c 'apt-get install -y aptitude'
-sudo apt-get install -y apache2
-sudo apt-get install -y postgresql postgresql-contrib
-sudo apt-get install -y libsasl2-dev python-dev libldap2-dev libssl-dev
-sudo apt-get install -y python-ldap gettext libapache2-mod-wsgi
-sudo apt-get install -y apache2-utils ssl-cert libapache2-mod-shib2
-sudo apt-get install -y ufw
-sudo apt-get install -y rabbitmq-server
-sudo apt-get install -y tmux
+activate(){
+	. ./venv/bin/activate
+	
+	sudo apt-get install -y apache2
+	sudo apt-get install -y postgresql postgresql-contrib
+	sudo apt-get install -y libsasl2-dev python-dev libldap2-dev libssl-dev
+	sudo apt-get install -y python-ldap gettext libapache2-mod-wsgi
+	sudo apt-get install -y apache2-utils ssl-cert libapache2-mod-shib2
+	sudo apt-get install -y ufw
+	sudo apt-get install -y rabbitmq-server
+	sudo apt-get install -y tmux
+	
+	sudo pip install south
+	sudo pip install uwsgi
+	sudo pip install httplib2
+	sudo pip install oauth2client
+	sudo pip install python_openid
+	
+	sudo apt update
+	sudo pip install -r requirements.txt
+	#sudo apt-get install -y postfix >> should be done before
+	
+	
+	sudo -u postgres createuser $(whoami)
+	sudo -u postgres createuser root
+	sudo -u postgres psql -c 'alter user '$(whoami)' with createdb;' postgres
+	sudo -u postgres createdb -O $(whoami) helios
+	echo Enviroment setup is Done!
+	sudo apt-get update
+	echo Updating...
+	sudo apt-get install -f
+	pip install -r requirements.txt
+	
 
-sudo pip install south
-sudo pip install uwsgi
-sudo pip install httplib2
-sudo pip install oauth2client
-sudo pip install python_openid
+	
+}
+activate
 
-sudo apt update
-sudo pip install -r requirements.txt
-sudo apt-get install -y postfix
-
-sudo -u debian createuser
-sudo -u createuser --superuser root
-sudo -u postgres createdb helios
-sudo ./reset.sh
-
-echo Done
+echo Reset database...
+sudo chmod 777 reset.sh
+sh -x ./reset.sh
+echo Installation Finished!
+echo ##################### 
+echo Add missing installation: sudo apt-get install -y postfix
+echo #####################
+echo Your Server IP: $(hostname -i)
+echo Run Command: python manage.py runserver 0.0.0.0:8000
 fi
