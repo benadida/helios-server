a21ab50051062217e39b1a2be9591f92cf1fac63
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2021-03-03 17:13:39 +0000

    Update supervisor - it works

--- deploy/apache/apache2-service.sh
+++ deploy/apache/apache2-service.sh
@@ -2,10 +2,13 @@
 if [[ `uname` == 'Linux' ]]; then
 cp /var/www/helios-server/deploy/apache/helios.conf /etc/apache2/sites-available
 sudo apachectl configtest
+sudo chmod 644 wsgi.py
 sudo a2enmod rewrite
 sudo a2dissite 000-default.conf
 sudo a2ensite helios.conf
+sudo a2enmod wsgi
 sudo systemctl reload apache2
 sudo chown www-data -R /var/www/helios-server
+sudo chown www-data:www-data /var/www/helios-server -R
 sudo chmod -R 750 /var/www/helios-server/*
 fi
--- deploy/supervisor/supervisor-service.sh
+++ deploy/supervisor/supervisor-service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ `uname` == 'Linux' ]]; then
+d="/var/log/celery"
 cp /var/www/helios-server/deploy/supervisor/celery-worker.conf /etc/supervisor/conf.d/
-mkdir /var/log/celery/
+[ -d "${d}" ] &&  echo "Directory $d found." ||  mkdir /var/log/celery/
 sudo chmod 766 /var/log/celery/
 sudo service supervisor start
 supervisorctl reread
