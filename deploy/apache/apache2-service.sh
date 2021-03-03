ac7d375e5a60e24db7f196688acfceba7d78a386
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2021-03-03 17:57:47 +0000

    Fix permissions apache deploy script

--- deploy/apache/apache2-service.sh
+++ deploy/apache/apache2-service.sh
@@ -8,7 +8,9 @@ sudo a2dissite 000-default.conf
 sudo a2ensite helios.conf
 sudo a2enmod wsgi
 sudo systemctl reload apache2
+sudo ufw status
+sudo systemctl status apache2.service
 sudo chown www-data -R /var/www/helios-server
 sudo chown www-data:www-data /var/www/helios-server -R
-sudo chmod -R 750 /var/www/helios-server/*
+sudo chmod -R 777 /var/www/helios-server/
 fi
