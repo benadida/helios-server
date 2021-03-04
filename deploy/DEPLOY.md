54e437c647c649c835be0450f9c60398b68b55d4
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2021-03-04 16:02:48 +0000

    Add more info for apache deploy script

--- deploy/DEPLOY.md
+++ deploy/DEPLOY.md
@@ -4,11 +4,11 @@
 To serve this Django App with Apache you will need to use WSGI and source a folder with all static files. Here is an example of deploying helios-server using raw bash script:
 
 ```bash
-    source .venv/bin/activate
     sudo chmod -R 777 /var/www
     sudo chmod o+u ./deploy/
     sudo chmod o+u ./deploy/*
-    sudo python2 manage.py collectstatic
+    source .venv/bin/activate
+    sudo -E python2 manage.py collectstatic
     sudo mkdir static/auth
     sudo mv static/login-icons/ static/auth
     sudo bash ./deploy/apache/apache2-service.sh
--- deploy/apache/apache2-service.sh
+++ deploy/apache/apache2-service.sh
@@ -2,24 +2,31 @@
 if [[ `uname` == 'Linux' ]]; then
 cp /var/www/helios-server/deploy/apache/helios.conf /etc/apache2/sites-available
 sudo apachectl configtest
+# Add access to Apache user (postgresql)
+echo 'Database configuration...'
+echo ''
 sudo adduser root  www-data
 sudo -u postgres createuser www-data
 sudo -u postgres psql -c 'alter user "www-data" with createdb;' postgres
 sudo -u postgres psql helios -c 'GRANT ALL PRIVILEGES ON ALL TABLES in SCHEMA public to "www-data";' postgres
 sudo -u postgres psql helios -c 'GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to "www-data";' postgres
 sudo -u postgres psql helios -c 'GRANT ALL PRIVILEGES ON ALL FUNCTIONS  IN SCHEMA public to "www-data";' postgres
-
 sudo systemctl restart postgresql
+# Enable new Apache configuration
+echo 'Apache configuration...'
+echo ''
 sudo chmod 644 wsgi.py
 sudo a2enmod rewrite
 sudo a2dissite 000-default.conf
 sudo a2ensite helios.conf
 sudo a2enmod wsgi
-sudo systemctl reload apache2
 sudo ufw status
-sudo systemctl reload apache2
+sudo systemctl restart apache2
+echo 'Apache configuration completed.'
+echo ''
 sudo systemctl status apache2.service
 sudo chown www-data -R /var/www/helios-server
 sudo chown www-data:www-data /var/www/helios-server -R
 sudo chmod -R 777 /var/www/helios-server/
+echo 'Configuration completed.'
 fi
