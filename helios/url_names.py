422ff3ede7fa4ce7edad044547ebef48046bebff
Ben Adida <ben@adida.net>
2021-10-06 16:39:45 +0000

    allow viewing a vote by full hash so we don't do quite as many DB queries.

--- helios/url_names.py
+++ helios/url_names.py
@@ -3,7 +3,7 @@ from helios import election_url_names as election, stats_url_names as stats
 __all__ = [
     "election", "stats",
     "COOKIE_TEST", "COOKIE_TEST_2", "COOKIE_NO",
-    "ELECTION_SHORTCUT", "ELECTION_SHORTCUT_VOTE", "CAST_VOTE_SHORTCUT",
+    "ELECTION_SHORTCUT", "ELECTION_SHORTCUT_VOTE", "CAST_VOTE_SHORTCUT", "CAST_VOTE_FULLHASH_SHORTCUT",
     "TRUSTEE_LOGIN",
     "ELECTIONS_PARAMS", "ELECTIONS_VERIFIER", "ELECTIONS_VERIFIER_SINGLE_BALLOT",
     "ELECTIONS_NEW", "ELECTIONS_ADMINISTERED", "ELECTIONS_VOTED",
@@ -16,6 +16,7 @@ COOKIE_NO="cookie@no"
 ELECTION_SHORTCUT="shortcut@election"
 ELECTION_SHORTCUT_VOTE="shortcut@election@vote"
 CAST_VOTE_SHORTCUT="shortcut@vote"
+CAST_VOTE_FULLHASH_SHORTCUT="shortcut-fullhash@vote"
 
 TRUSTEE_LOGIN="trustee@login"
 
