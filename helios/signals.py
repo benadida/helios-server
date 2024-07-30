6ce880603967d31b2d40c74e50e07051bf6ecf34
Lucas Araujo <8809676+laoumh@users.noreply.github.com>
2024-07-30 10:07:17 -0300

    Upgrade Django 3.0 to 4.0: The 'providing_args' argument for django.dispatch.Signal is removed

--- helios/signals.py
+++ helios/signals.py
@@ -1,16 +1,21 @@
 """
 Helios Signals
 
-Effectively callbacks that other apps can wait and be notified about
+Effectively callbacks that other apps can wait and be notified about.
+
+Named arguments for each signal:
+    election_created : "election"
+    vote_cast : "user", "voter", "election", "cast_vote"
+    election_tallied : "election"
 """
 
 import django.dispatch
 
 # when an election is created
-election_created = django.dispatch.Signal(providing_args=["election"])
+election_created = django.dispatch.Signal()
 
 # when a vote is cast
-vote_cast = django.dispatch.Signal(providing_args=["user", "voter", "election", "cast_vote"])
+vote_cast = django.dispatch.Signal()
 
 # when an election is tallied
-election_tallied = django.dispatch.Signal(providing_args=["election"])
+election_tallied = django.dispatch.Signal()
